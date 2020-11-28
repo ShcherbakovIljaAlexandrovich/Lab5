a8584ed428b596f0477d1a1793b2a9d4cfbbfa86
Ilja <ilja@ylyxa.ylyxa.com>
2020-11-28 13:13:02 +0300

    Scripted auto-commit on change (2020-11-28 13:13:02) by gitwatch.sh

--- src/main/java/TesterActor.java
+++ src/main/java/TesterActor.java
@@ -1,2 +1,14 @@
-public class TestActor {
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.HashMap;
+
+public class TesterActor extends AbstractActor {
+    private final HashMap<String, Integer> storage = new HashMap<>();
+
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(GetMessage.class)
+    }
 }
