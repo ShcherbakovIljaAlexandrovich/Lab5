3f776c60d3d737ed7f668b292dd4216db8e7517b
Ilja <ilja@ylyxa.ylyxa.com>
2020-11-28 13:33:13 +0300

    Scripted auto-commit on change (2020-11-28 13:33:13) by gitwatch.sh

--- src/main/java/StressTester.java
+++ src/main/java/StressTester.java
@@ -1,4 +1,5 @@
 import akka.NotUsed;
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
@@ -6,14 +7,19 @@ import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.model.Query;
+import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+import akka.util.Timeout;
 import javafx.util.Pair;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.concurrent.CompletionStage;
 
 public class StressTester {
+    private static final Timeout timeout = Timeout.create(Duration.ofSeconds(5));
+
     public static void main(String[] args) throws IOException {
         System.out.println("start!");
         ActorSystem system = ActorSystem.create("routes");
@@ -33,7 +39,8 @@ public class StressTester {
                 .thenAccept(unbound -> system.terminate());
     }
 
-    private static Flow<HttpRequest, HttpResponse, NotUsed> createFlow(ActorMaterializer materializer) {
+    private static Flow<HttpRequest, HttpResponse, NotUsed> createFlow(ActorMaterializer materializer,
+                                                                       ActorRef cachingActor) {
         return Flow.of(HttpRequest.class)
                 .map((req) -> {
                     Query q = req.getUri().query();
@@ -42,6 +49,7 @@ public class StressTester {
                     return new Pair<>(url, count);
                 })
                 .mapAsync(1, (Pair<String, Integer> p) -> {
+                    CompletionStage<Object> stage = Patterns.ask(cachingActor, new GetMessage(p.getKey()), timeout);
 
                 })
     }
