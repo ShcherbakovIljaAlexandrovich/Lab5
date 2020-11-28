052bd9d3b363533342874db17af96f91d4e9e447
Ilja <ilja@ylyxa.ylyxa.com>
2020-11-28 13:10:45 +0300

    Scripted auto-commit on change (2020-11-28 13:10:45) by gitwatch.sh

--- src/main/java/StressTester.java
+++ src/main/java/StressTester.java
@@ -1,2 +1,48 @@
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.model.Query;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import javafx.util.Pair;
+
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+
 public class StressTester {
+    public static void main(String[] args) throws IOException {
+        System.out.println("start!");
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer =
+                ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = createFlow(materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
+    }
+
+    private static Flow<HttpRequest, HttpResponse, NotUsed> createFlow(ActorMaterializer materializer) {
+        return Flow.of(HttpRequest.class)
+                .map((req) -> {
+                    Query q = req.getUri().query();
+                    String url = q.get("testUrl").get();
+                    int count = Integer.parseInt(q.get("count").get());
+                    return new Pair<>(url, count);
+                })
+                .mapAsync(1, (Pair<String, Integer> p) -> {
+
+                })
+    }
 }
